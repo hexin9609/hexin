178e5949a7db8b65b5fbb4aa8486df217cf1c46f
hexin9609 <hexin09180616@gmail.com>
2025-01-03 10:53:13 +0800

    Update sing box.js

--- sing box.js
+++ sing box.js
@@ -43,6 +43,9 @@ config.outbounds.map(i => {
   if (['🌐 其它地区'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:香港|台湾|日本|新加坡)).*()/i))
   }
+  if (['chatgpt'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:Fly 香港|Fly 新加坡|Fly 台湾|美国|英国|土耳其|阿根廷|三分|牛逼 新加坡|牛逼 日本|牛逼 韩国|牛逼 台湾|加拿大|飞鸟 新加坡|飞鸟 日本|飞鸟 香港)).*()/i))
+  }
 })
 
 config.outbounds.forEach(outbound => {
